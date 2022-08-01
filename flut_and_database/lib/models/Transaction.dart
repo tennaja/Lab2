4abac955c2341be5d5242f1fb36e376ffc1b2368
tennaja <ten446654@gmail.com>
2022-08-01 17:43:00 +0700

    create provider

---
+++ flut_and_database/lib/models/Transaction.dart
@@ -0,0 +1,8 @@
+
+class Transaction {
+  String title;
+  double amount;
+  DateTime date;
+
+  Transaction ({this.title, this.amount, this.date}) ;
+}
---
+++ flut_and_database/lib/providers/transaction_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flut_and_database/models/Transaction.dart';
+import 'package:flutter/foundation.dart';
+
+class TransactionProvider with ChangeNotifier{
+  
+  List<Transaction> transaction = [
+
+    Transaction(title : "ชื่อหนังสือ",amount : 500,date:DateTime.now()),
+     Transaction(title : "เสื้อผ้า",amount : 400,date:DateTime.now()),
+      Transaction(title : "กางเกง",amount : 300,date:DateTime.now()),
+  ];
+  List<Transaction>getTransaction(){
+    return transaction;
+  }
+
+  addTransaction( Transaction statement){
+    transaction.add(statement);
+  }
+}
